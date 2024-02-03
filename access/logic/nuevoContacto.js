e6e26fc73f32b9b63d48796727f4077399e2eee5
jrevolorio2019058 <jrevolorio-2019058@kinal.edu.gt>
2024-02-03 08:33:43 -0600

    Creando estructura básica Nuevo Contacto

---
+++ access/logic/nuevoContacto.js
@@ -0,0 +1,34 @@
+function obtener(){
+    let abrirModal = document.getElementById('abrirModal');
+    let modal = document.getElementById('modal');
+    let siBtn = document.getElementById('siBtn');
+    let noBtn = document.getElementById('noBtn');
+    let modalAbierto = true;
+}
+
+function abrirModal(){
+
+    obtener();
+    modalAbierto = true;
+    modal.style.display = 'flex';
+}
+
+function negativoSalir(){
+    obtener();
+    modalAbierto = false;
+    modal.style.display = 'none';
+}
+
+function positivoSalir(){
+    obtener();
+    modalAbierto = false;
+    window.location.href = './../../index.html';
+    modal.style.display = 'none';
+}
+
+window.addEventListener('beforeunload', function (event) {
+    if (modal.style.display === 'flex' && modalAbierto) {
+        event.preventDefault();
+        event.returnValue = '¿Seguro de Salir?';
+    }
+  });
